949a052d2c4bf4ee1a205af471b847fe9ad3dedf
YoanaAneva <92211354+YoanaAneva@users.noreply.github.com>
2021-10-28 23:43:07 +0300

    Add files via upload

--- week3/Homework/triangle.cpp
+++ week3/Homework/triangle.cpp
@@ -3,37 +3,33 @@ using namespace std;
 int main(){
     int a, b, c;
     cout << "Enter triangle sides: ";
-    cin >> a, b, c;
-    // bool isTriangle = (a < (b + c)) && 
-    //                   (b < (a + c)) &&
-    //                   (c < (a + b));
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << a << endl << b << endl << c << endl;
+    bool isTriangle = (a < (b + c)) && 
+                      (b < (a + c)) &&
+                      (c < (a + b));
 
-    bool x = (a < (b + c));
-    bool y = (b < (a + c));
-    bool z = (c < (a + b));
-    
-
-    cout << x << endl << y << endl << z << endl;
+    if (isTriangle == 1){
+        
+            cout << "exists, ";
+        if (a == b && b == c)
+        {
+             cout << "equilateral" << endl;
+        }
+        else if (a == b && a != c) 
+        {
+            cout << "isoceles" << endl;
+        }
+        else
+        {
+            cout << "scalene";
+        }
+     } 
+     else 
+     {
+         cout << "does not exist!";
+     }                 
+    return 0;
 }
-//     if (isTriangle = 1)
-//     {
-//         cout << "exists";
-//         if (a == b && b == c)
-//         {
-//              cout << "equilateral" << endl;
-//         }
-//         else if (a == b && a != c) 
-//         {
-//             cout << "isoceles" << endl;
-//         }
-//         else
-//         {
-//             cout << "scalene";
-//         }
-//      } 
-//      else 
-//      {
-//          cout << "does not exist!";
-//      }                 
-// return 0;
-// }
